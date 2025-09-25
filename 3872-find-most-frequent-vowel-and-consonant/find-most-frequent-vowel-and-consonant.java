339633c7ddc793200fccbccc21348fc3d8ac3f33
Harsh Raj <149399764+HarshR082@users.noreply.github.com>
2025-09-26 00:31:59 +0530

    Time: 1 ms (100.00%) | Memory: 43 MB (11.62%) - LeetSync

---
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxFreqSum(String s) {
+        int[] arr = new int[123];
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            arr[c-0]++;
+        }
+        int maxv=0;
+        int maxc=0;
+        for(int i=97;i<arr.length;i++){
+            if(i==97||i==101||i==105||i==111||i==117){
+                maxv=Math.max(arr[i],maxv);
+            }else{
+                maxc=Math.max(arr[i],maxc);
+            }
+        }
+        return maxv+maxc;
+    }
+}
