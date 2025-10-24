db1c19b3577c23730be161521225fcebc0726c35
Harsh Raj <149399764+HarshR082@users.noreply.github.com>
2025-10-24 12:37:37 +0530

    Time: 142 ms (23.85%) | Memory: 44.5 MB (44.62%) - LeetSync

---
+++ 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int nextBeautifulNumber(int n) {
+        boolean isfound=false;
+        for(int i=n+1;i<999999999;i++){
+            if(isnum(i)){
+                return i;
+            }
+        }
+        return -1;
+    }
+    private boolean isnum(int k){
+        int[] arr = new int[10];
+        int temp=k;
+        while(temp>0){
+            int d = temp%10;
+            arr[d]++;
+            temp=temp/10;
+        }
+        String s = String.valueOf(k);
+        for(int i=0;i<s.length();i++){
+            int a = s.charAt(i)-'0';
+            int b = arr[a];
+            if(a!=b) return false;
+        }
+        return true;
+    }
+}
