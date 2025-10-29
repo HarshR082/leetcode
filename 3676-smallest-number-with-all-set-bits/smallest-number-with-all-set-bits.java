8b01eaf8be5a1bb5f543ad76a18e1f74ef136ac3
Harsh Raj <149399764+HarshR082@users.noreply.github.com>
2025-10-29 22:35:21 +0530

    Time: 6 ms (4.19%) | Memory: 43.9 MB (5.24%) - LeetSync

---
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int smallestNumber(int n) {
+        while(true){
+            String s = Integer.toBinaryString(n);
+            if(s.contains("0")) n++;
+            else{
+                //return n;
+                break;
+
+            }
+        }
+        return n;
+    }
+}
