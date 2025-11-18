ad3ee6260b38a60c5aa76237d14f605af47c680e
Harsh Raj <149399764+HarshR082@users.noreply.github.com>
2025-11-18 21:38:26 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (22.02%) - LeetSync

---
+++ 717-1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isOneBitCharacter(int[] bits) {
+        int n = bits.length;
+        for(int i=0;i<n;i++){
+            if(bits[i]==1){
+                i+=1;
+                continue;
+            }
+            if(bits[i]==0 && i==n-1) return true;
+        }
+        return false;
+    }
+}
