5928b3bdd459d8c6b9dd9e270048edc5fb4f6990
Harsh Raj <149399764+HarshR082@users.noreply.github.com>
2026-02-25 14:23:24 +0530

    Time: 8 ms (61.40%) | Memory: 46.9 MB (39.38%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
@@ -1,13 +1,24 @@
 class Solution {
     public int[] sortByBits(int[] arr) {
-        Integer[] temp = Arrays.stream(arr).boxed().toArray(Integer[] :: new);
+
+        //DIRECT METHOD TO CONVERT INTO INTEGER ARRAY 
+        // Integer[] temp = Arrays.stream(arr).boxed().toArray(Integer [] :: new);
+        Integer[] temp = new Integer[arr.length];
+        for(int i=0;i<arr.length;i++){
+            temp[i]=arr[i];
+        }
         Arrays.sort(temp,(a,b) -> {
             int bita = Integer.bitCount(a);
             int bitb = Integer.bitCount(b);
             if(bita==bitb) return a-b;
             return bita-bitb;
         });
-        arr = Arrays.stream(temp).mapToInt(i -> i).toArray();
+
+        // DIRECT METHOD TO CONVERT INTEGER ARRAY TO INT ARRAY
+        // arr = Arrays.stream(temp).mapToInt(i -> i).toArray();
+        for(int i=0;i<arr.length;i++){
+            arr[i]=temp[i];
+        }
         return arr;
     }
 }
